524f6862ccbd19dee493e2f988011f45c7b9b470
userpages <145786854+userpages@users.noreply.github.com>
2024-03-06 15:26:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -170,8 +170,13 @@ setInterval(function() {
 }, 1000); // 1000 ms = 1 second
 
 setInterval(function() {
-    if(superMiUpgrades & miUpgrades > 0) {
+    if(superMiUpgrades > 0) {
         createParticle();
     }
 }, 5000); // 5000 ms = 5 seconds
 
+setInterval(function() {
+    if(miUpgrades > 0) {
+        createParticle();
+    }
+}, 5000); // 5000 ms = 5 seconds
